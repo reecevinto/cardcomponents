49f8cfb3af0fbc29c6eaac4ede16415066f3572b
Collins Murgor <vintoncollins702@gmail.com>
2023-07-24 14:38:03 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import "./styles.css";
+
+function Card({ children }) {
+  return (
+    <div className="card">
+      <div className="card-content">{children}</div>
+    </div>
+  );
+}
+
+export default function scientistProfile() {
+  return (
+    <div>
+      <Card>
+        <h1>Photo</h1>
+        <img
+          className="avatar"
+          src="https://i.imgur.com/OKS67lhm.jpg"
+          alt="Aklilu Lemma"
+          width={100}
+          height={100}
+        />
+      </Card>
+      <Card>
+        <h1>About</h1>
+        <p>
+          Aklilu Lemma was a distinguished Ethiopian Scientist who discovered a
+          natural treatment to schistosomiasis
+        </p>
+      </Card>
+    </div>
+  );
+}
